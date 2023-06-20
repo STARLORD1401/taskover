543defb7d4cb43c83ef008b611637099eb45844f
STARLORD1401 <shreyaspjoshi.14@gmail.com>
2023-06-20 11:41:56 +0530

    Testing response

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,6 @@ app.use(express.json());
 app.use("/", index, () => {
   try {
     console.log("test me");
-    mongoose.connect(process.env.TASKOVER_URI);
-    console.log(`Connected to ${process.env.PORT}`);
-    console.log("Connected to MongoDB");
   } catch (err) {
     console.log(err);
   }
