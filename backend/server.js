7cceaf452bfcc6e2721e239a50152568b84a6ecd
STARLORD1401 <shreyaspjoshi.14@gmail.com>
2023-06-20 12:15:39 +0530

    Testing backend 3

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 import express from "express";
 import cors from "cors";
 import index from "./routes/index.js";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
-
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.listen(process.env.PORT);
+mongoose.connect(process.env.TASKOVER_URI).then(
+  app.listen(process.env.PORT, () => {
+    console.log(
+      `Connected to MongoDB and listening on port: ${process.env.PORT}`
+    );
+  })
+);
 app.use(express.json());
-app.use("/", index, () => {
-  console.log("test me");
-});
+app.use("/", index);
 app.use("*", (req, res) => res.status(404).json({ ERROR: "Not Found" }));
 export default app;
